fb34939abb361a48056d98d57a77a9c7e54293b9
Rufei <xuancheng@brettyang.com>
2016-11-06 10:39:56 -0600

    Add goal canvas structure.

---
+++ byang/ghostbird/Assets/Scripts/GoalCanvasDriver.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GoalCanvasDriver : MonoBehaviour {
+
+    public Image sun;
+    public Image cloud;
+    private bool isActive;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isActive)
+        {
+            if (Input.GetMouseButton(1))
+            {
+                ShowGoalCanvas(true);
+            }
+        }
+	}
+
+    void ShowGoalCanvas(bool shouldShow)
+    {
+        if (shouldShow)
+        {
+
+        }
+    }
+}
