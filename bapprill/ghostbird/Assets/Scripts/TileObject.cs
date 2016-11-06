34ce66061bb9a2a46954715e497aeb499403b2a8
bsapprill <bsapprill@fullsail.edu>
2016-11-06 11:35:38 -0600

    wiping for re load

--- bapprill/ghostbird/Assets/Scripts/TileObject.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public abstract class TileObject : MonoBehaviour
-{
-    public abstract void OnMouseEnter();
-    public abstract void OnMouseExit();
-    public abstract void OnMouseDown();
-}
