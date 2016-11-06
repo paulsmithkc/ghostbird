f8f047fe79294cf0fc03126035ce9fb3d528a9ca
Rufei <xuancheng@brettyang.com>
2016-11-06 12:12:18 -0600

    Add Act 2 level, add additional GoalCanvasDriver code

--- byang/ghostbird/Assets/Scripts/GoalCanvasDriver.cs
+++ byang/ghostbird/Assets/Scripts/GoalCanvasDriver.cs
@@ -4,8 +4,19 @@ using UnityEngine.UI;
 
 public class GoalCanvasDriver : MonoBehaviour {
 
+    public Canvas canvas;
+
+    public Image sky;
     public Image sun;
     public Image cloud;
+
+    public Sprite smallSun;
+    public Sprite medSun;
+    public Sprite bigSun;
+
+    public Sprite partCloud;
+    public Sprite fullCloud;
+
     private bool isActive;
 
 	// Use this for initialization
@@ -26,9 +37,71 @@ public class GoalCanvasDriver : MonoBehaviour {
 
     void ShowGoalCanvas(bool shouldShow)
     {
-        if (shouldShow)
+        if (canvas.enabled != shouldShow)
         {
-
+            //SetDistance(stage)
+            //SetTemperature(stage)
         }
+        canvas.enabled = shouldShow;
+    }
+
+    void SetDistance(int stage)
+    {
+        switch (stage)
+        {
+            case 1:
+                sun.sprite = smallSun;
+                break;
+            case 2:
+                sun.sprite = medSun;
+                break;
+            case 3:
+                sun.sprite = bigSun;
+                break;
+            default:
+                sun.sprite = smallSun;
+                break;
+        }
+    }
+
+    void SetTemperature(int stage)
+    {
+        Color tint = Color.white;
+        Color sunColor = sun.color;
+        switch (stage)
+        {
+            case 1:
+                cloud.enabled = false;
+                sunColor.a = 1f;
+                break;
+            case 2:
+                cloud.enabled = false;
+                sunColor.a = .8f;
+                break;
+            case 3:
+                cloud.enabled = false;
+                sunColor.a = .5f;
+                break;
+            case 4:
+                cloud.enabled = true;
+                sunColor.a = .3f;
+                cloud.sprite = partCloud;
+                break;
+            case 5:
+                cloud.enabled = true;
+                sunColor.a = .15f;
+                cloud.sprite = fullCloud;
+                break;
+            default:
+                break;
+        }
+
+        sunColor.r = tint.r;
+        sunColor.g = tint.g;
+        sunColor.b = tint.b;
+
+        sky.color = tint;
+        sun.color = sunColor;
+        cloud.color = tint;
     }
 }
