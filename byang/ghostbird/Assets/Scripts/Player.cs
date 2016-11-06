bf9c2c5aba27cc465561cb795d665486be31261d
Rufei <xuancheng@brettyang.com>
2016-11-06 06:43:59 -0600

    Initial commit of Act 1 implementation

---
+++ byang/ghostbird/Assets/Scripts/Player.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Player : MonoBehaviour
+{
+    // Inputs
+    public const string HORIZONTAL_INPUT = "Horizontal";
+    public const string VERTICAL_INPUT = "Vertical";
+
+    // Tags
+    public const string PLAYER_TAG = "Player";
+    public const string PREDATOR_TAG = "Predator";
+
+    // Physicis Tuning
+    private const float _moveSpeed = 2.0f;
+    private const float _tiredMax = 15.0f;
+    private const float _sleepMultiplier = -2.0f;
+
+    // Game State
+    //[HideInInspector]
+    public float _tiredCurrent = 0.0f;
+    [HideInInspector]
+    public bool _sleeping = false;
+    [HideInInspector]
+    public List<Tile> _path = null;
+    [HideInInspector]
+    public Tile _originTile = null;
+    [HideInInspector]
+    public Tile _targetTile = null;
+
+    // Designer
+    private Rigidbody _rigidbody;
+    private Animator _animator;
+
+    void Start()
+    {
+        if (!_rigidbody)
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+        if (!_animator)
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        _tiredCurrent = 0.0f;
+        _sleeping = false;
+        _path = null;
+        _originTile = null;
+        _targetTile = null;
+    }
+
+    void Update()
+    {
+        float deltaTime = Time.deltaTime;
+
+        _sleeping = _sleeping ^ Input.GetButtonDown("Fire2");
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            _sleeping = false;
+            _animator.SetTrigger("eating");
+        }
+
+        _tiredCurrent = Mathf.Clamp(
+            _tiredCurrent + (_sleeping ? deltaTime * _sleepMultiplier : deltaTime),
+            0.0f, _tiredMax
+        );
+    }
+
+    void FixedUpdate()
+    {
+        float deltaTime = Time.fixedDeltaTime;
+
+        //float horizontal = Input.GetAxis(HORIZONTAL_INPUT);
+        //float vertical = Input.GetAxis(VERTICAL_INPUT);
+        /*
+        _rigidbody.velocity = new Vector3(
+            horizontal * _moveSpeed,
+            0.0f,
+            vertical * _moveSpeed
+        );
+        */
+
+        bool walking = false;
+        if (_path != null && _path.Count > 0)
+        {
+            Vector3 currentPos = transform.position;
+            Vector3 nextPos = currentPos;
+            float moveDistance = 0.0f;
+            do
+            {
+                nextPos = _path[0].transform.position;
+                moveDistance = Vector3.Distance(currentPos, nextPos);
+                if (moveDistance <= 0.5f)
+                {
+                    _path.RemoveAt(0);
+                }
+                else
+                {
+                    break;
+                }
+            } while (_path.Count > 0);
+
+            if (moveDistance > 0.5f)
+            {
+                if (_moveSpeed < moveDistance)
+                {
+                    moveDistance = _moveSpeed;
+                }
+                Vector3 moveVector = 
+                    (_moveSpeed < moveDistance) ?
+                    (nextPos - currentPos) :
+                    (nextPos - currentPos).normalized * _moveSpeed;
+
+                _rigidbody.MovePosition(currentPos + moveVector * deltaTime);
+                _rigidbody.velocity = Vector3.zero;
+                _sleeping = false;
+                walking = true;
+
+                if (moveVector.x > 0.0f)
+                {
+                    transform.localScale = Vector3.one;
+                }
+                else if (moveVector.x < 0.0f)
+                {
+                    transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                }
+            }
+        }
+
+        _animator.SetBool("walking", walking);
+        _animator.SetBool("sleeping", _sleeping);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+
+        if (_originTile != null)
+        {
+            Gizmos.DrawSphere(_originTile.transform.position, 0.2f);
+        }
+        if (_targetTile != null)
+        {
+            Gizmos.DrawSphere(_targetTile.transform.position, 0.2f);
+        }
+
+        if (_path != null)
+        {
+            Vector3 currentPos = transform.position;
+            foreach (var tile in _path)
+            {
+                Vector3 nextPos = tile.transform.position;
+                Gizmos.DrawLine(currentPos, nextPos);
+                currentPos = nextPos;
+            }
+        }
+    }
+}
---
+++ byang/ghostbird/Assets/Scripts/Tile.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class Tile : MonoBehaviour {
+
+    // Game State
+    public int _predatorOccupancy = 0;
+
+    // Designer
+    public float _width = 1.0f;
+    public float _height = 1.0f;
+    public AdjacenctRoom[] _adjacentRooms = new AdjacenctRoom[0];
+
+    [System.Serializable]
+    public class AdjacenctRoom {
+        public GameObject door;
+        public GameObject room;
+    }
+    
+    void Start () {
+        _predatorOccupancy = 0;
+    }
+
+    void OnMouseDown()
+    {
+        var sector = GameObject.FindObjectOfType<Sector>();
+        var player = GameObject.FindObjectOfType<Player>();
+
+        player._originTile = this;
+        player._targetTile = sector.GetClosestTile(player.transform.position);
+        player._path = sector.FindShortestPath(this, player._targetTile, 100);
+
+        Debug.Log("down");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case Player.PREDATOR_TAG:
+                ++_predatorOccupancy;
+                break;
+            case Player.PLAYER_TAG:
+                //--_predatorOccupancy;
+                break;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        var roomCenter = transform.position;
+        Gizmos.matrix = Matrix4x4.TRS(
+            roomCenter,
+            transform.localRotation,
+            Vector3.one
+        );
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(
+            Vector3.zero, 
+            new Vector3(_width, _height, 0.5f + 0.5f * _predatorOccupancy)
+        );
+        //Gizmos.DrawSphere(Vector3.zero, 0.2f);
+
+        /*
+        Gizmos.color = Color.blue;
+        foreach (var adj in _adjacentRooms)
+        {
+            if (adj.room != null)
+            {
+                Gizmos.DrawLine(Vector3.zero, adj.room.transform.position - roomCenter);
+            }
+        }
+        */
+    }
+}
