6bca0338117d9bc42fd8825b79249ff2543381e0
Rufei <xuancheng@brettyang.com>
2016-11-06 08:29:39 -0600

    Added MagicCliffArtwork assets. Added Act 1 events.

--- byang/ghostbird/Assets/Scripts/IntroAct1.cs
+++ byang/ghostbird/Assets/Scripts/IntroAct1.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class IntroAct1 : MonoBehaviour {
 
@@ -7,6 +8,9 @@ public class IntroAct1 : MonoBehaviour {
     public Transform extremeCloseCameraSnapTransform;
     public Transform fullLevelCameraSnapTransform;
     public Transform playerTransform;
+    public Image introspectionContainer;
+    public Text introspectionText;
+    public HudFade fader;
     private Transform cameraTransform;
     private float timeElapsed;
 
@@ -15,21 +19,73 @@ public class IntroAct1 : MonoBehaviour {
         cameraTransform = Camera.main.transform;
         cameraTransform.position = eyeShotCameraSnapTransform.position;
         timeElapsed = 0f;
+        introspectionContainer.enabled = true;
+        introspectionContainer.gameObject.SetActive(true);
+        introspectionText.enabled = true;
+        introspectionText.gameObject.SetActive(true);
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
         timeElapsed += Time.deltaTime;
-        if (timeElapsed <= 1f)
-        {
 
-        } else if (timeElapsed < 3f)
+        DoIntrospection();
+        DoFadeInFromWhite();
+        DoZoomOutToShowBigBird();
+        DoZoomOutToShowBirdsFlyingAndLevelContext();
+        DoGiveGoalScreenHint();
+    }
+
+    void DoIntrospection()
+    {
+        string introspectionStr = "";
+
+        if (timeElapsed >= 0f && timeElapsed < 3f)
         {
-            MoveCamera(eyeShotCameraSnapTransform, extremeCloseCameraSnapTransform, 3f);
-        } else if (timeElapsed > 4f && timeElapsed < 5f)
+            introspectionStr = "My love came late. My child came late.";
+        }
+        else if (timeElapsed >= 3f && timeElapsed < 6f)
         {
-            MoveCamera(extremeCloseCameraSnapTransform, fullLevelCameraSnapTransform, 5f);
+            introspectionStr = "Do I regret? I do not know. There is no time to think.";
+        }
+        else if (timeElapsed >= 6f && timeElapsed < 9f)
+        {
+            introspectionStr = "I can feel it in the air.";
+        }
+        else if (timeElapsed >= 9f && timeElapsed < 12f)
+        {
+            introspectionStr = "Winter approaches.";
+        }
+
+        introspectionContainer.enabled = introspectionStr.Length > 0;
+        introspectionContainer.gameObject.SetActive(introspectionStr.Length > 0);
+        introspectionText.enabled = introspectionStr.Length > 0;
+        introspectionText.gameObject.SetActive(introspectionStr.Length > 0);
+        introspectionText.text = introspectionStr;
+    }
+
+    void DoFadeInFromWhite()
+    {
+        if (timeElapsed >= 12f && timeElapsed < 13f)
+        {
+            fader.FadeTo(new Color(255, 255, 255, 0), 1f);
+        }
+    }
+
+    void DoZoomOutToShowBigBird()
+    {
+        if (timeElapsed >= 13f && timeElapsed < 16f)
+        {
+            MoveCamera(eyeShotCameraSnapTransform, extremeCloseCameraSnapTransform, 16f);
+        }
+    }
+
+    void DoZoomOutToShowBirdsFlyingAndLevelContext()
+    {
+        if (timeElapsed >= 17f && timeElapsed < 18f)
+        {
+            MoveCamera(extremeCloseCameraSnapTransform, fullLevelCameraSnapTransform, 18f);
         }
     }
 
@@ -37,5 +93,11 @@ public class IntroAct1 : MonoBehaviour {
     {
         cameraTransform.position = Vector3.Lerp(to.position, from.position, endTime - timeElapsed);
     }
-    
+
+    void DoGiveGoalScreenHint()
+    {
+
+    }
+
+
 }
